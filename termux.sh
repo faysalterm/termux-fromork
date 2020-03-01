0df7de5720c4208034454a1586cd685f8694bcdf
faysalterm <61668343+faysalterm@users.noreply.github.com>
2020-03-01 23:55:19 +0100

    Create termux.sh

---
+++ termux.sh
@@ -0,0 +1,188 @@
+#!/bin/bash
+#My name is Faysal, I love programming
+
+pkg update
+pkg upgrade
+termux-setup-storage
+pkg install python -y
+pkg install python2 -y
+pkg install python3 -y
+pkg install ruby -y
+pkg install git -y
+pkg install php -y
+pkg install java -y
+Pkg install google -y
+pkg install bash -y
+pkg install perl -y
+pkg install nmap -y
+pkg install bash -y
+pkg install clang -y
+pkg install macchanger -y
+pkg install nano -y
+pkg install figlet -y
+pkg install cowsay -y
+pkg install curl -y
+pkg install tar -y
+pkg install zip -y
+pkg install unzip -y
+pkg install tor -y
+pkg install sudo -y
+pkg install wget -y
+pkg install wcalc -y
+pkg install openssl -y
+pkg install bmon -y
+pkg install man vim -y
+pkg install w3m -y
+pkg install file -y
+#-------_-----_---++-+---_
+# facebook
+apt update && apt upgrade
+pkg install git figlet toilet python2
+pip2 install lolcat
+git clone https://github.com/hatakecnk/xNot_Found.git
+cd xNot_Found
+chmod +x xNot_Found.py
+cd ~
+#-------
+apt update && apt upgrade
+pkg install git python2
+git clone https://github.com/hatakecnk/CompileMarshal
+cd CompileMarshal
+cd ~
+#Hack Facebook Target
+apt update && apt upgrade
+pkg install git python2
+pip2 install mechanize
+git clone https://github.com/hatakecnk/FB-Cracker
+#........
+#Untuk Melacak Lokasi Melalui IP
+apt update && apt upgrade
+pkg install git python2
+pip2 install requests
+git clone https://github.com/hatakecnk/CNK-IPFinder
+#------
+#Tool CNK-ReverseIP*
+#Find Domains On The Same Server
+apt update && apt upgrade
+pkg install git python2
+pip2 install requests
+git clone https://github.com/hatakecnk/CNK-ReverseIP
+#--------
+#Tool CNK-SSH
+#Auto Create SSH Account
+apt update && apt upgrade -y
+pkg install git php -y
+git clone https://github.com/hatakecnk/CNK-SSH
+#..... 
+#Tool CNK-Shortlink*
+#Memperpendek alamat url goo.gl
+apt update && apt upgrade -y
+pkg install git php
+git clone https://github.com/hatakecnk/CNK-Shortlink
+#-----
+#Tool CNK-Shrink*
+#Spam Call Unlimited!
+apt update && apt upgrade -y
+pkg install git python2 -y
+pip2 install requests
+git clone https://github.com/hatakecnk/Call-Spam
+#----------
+#TOOL CNK-DDOS*
+apt update && apt upgrade -y
+pkg install git python2
+git clone https://github.com/hatakecnk/CNK-DDOS
+#--------------
+#Tool Bot-Caping*
+pkg install php git
+git clone https://github.com/hatakecnk/bot-caping
+#--------
+#Tool Bot-YDNews
+pkg install php git
+git clone https://github.com/hatakecnk/Bot-YDNews
+#-----+-+-
+#Tools Gempa-Terkini*
+#LiveInfoGempa
+apt update && apt upgrade -y
+pkg install git python2
+pip2 install requests
+https://github.com/hatakecnk/Gempa-Terkini
+#----------
+#Spammer Whatsapp
+apt update && apt upgrade -y
+pkg install git php
+git clone https://github.com/hatakecnk/waspam
+#-----
+#Pastebin In Termux
+#Mempermudah Para COPASer Untuk Akses
+#Pastebin ditermux tanpa ribet² buka googl
+apt update && apt upgrade -y
+pkg install git php
+git clone https://github.com/hatakecnk/Pastebin
+#--------
+#CARA DEFACE WEB POC WEBDAV VIA
+#TERMUX
+#Lakukan Comand Berikut:
+cd /storage/emulated/0/
+pkg install git python2 figlet
+git clone https://github.com/hatakecnk/webdav
+#________
+pkg install python2-dev python2 git -y
+get clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+pip2 install -r requirements.txt
+cd ~
+#____________
+git clone https://github.com/IceM4nn/IP-Obfuscator
+cd  IP-Obfuscator
+chmod +x *
+cd ~
+#-------------------
+git clone https://github.com/siputra12/fakecall
+#---
+#::ngrok::
+./ngrok authtoken 5YBE4yiY4t1XREWwFWZMD_69Vt29dFhZ3XNF96gT4Pk
+###-------
+##metasploit---+
+echo mitasploite 
+echo '#!/bin/bash' > ploit.txt
+echo 'apt-get update&&upgrade -y' >> ploit.txt
+echo 'pkg install ruby&&gem install bundler' >> ploit.txt
+echo 'gem install bundle&&pip install bundle' >> ploit.txt
+echo 'pip install --upgrade pip' >> ploit.txt
+echo 'pkg install wget' >> ploit.txt
+echo 'wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/metasploit.sh' >> ploit.txt
+echo 'chmod +x metasploit.sh' >> ploit.txt
+echo './metasploit.sh' >> ploit.txt
+chmod +x ploit.txt
+echo Ok OK
+#----------
+echo '#!/bin/bash' > postgresql.sh
+echo 'mkdir -p $PREFIX/var/lib/postgresql' >> postgresql.sh
+echo 'initdb $PREFIX/var/lib/postgresql' >> postgresql.sh
+echo 'pg_ctl -D $PREFIX/var/lib/postgresql start' >> postgresql.sh
+chmod +x postgresql.sh
+#----------------
+#tumxbunsh
+git clone https://github.com/Hax4us/Tmux-Bunch.git
+clear
+sleep 3;echo louati faysal
+cd Tmux-Bunch
+sleep 2;chmod +x *
+cd ~
+#-------
+#shellphish
+git clone https://github.com/thelinuxchoice/shellphish.git
+cd shellphish
+chmod +x *
+cd ~
+clear
+date
+#-------
+#AHT-hacher
+
+
+
+
+
+
+
